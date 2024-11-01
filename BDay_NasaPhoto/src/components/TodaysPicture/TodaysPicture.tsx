013f74b08e8b5eaa8a4fce074cd5809e15691251
José <zecarlosnetodantas@gmail.com>
2024-11-01 16:46:10 -0300

    Fix: Today's page h1 fixed

--- BDay_NasaPhoto/src/components/TodaysPicture/TodaysPicture.tsx
+++ BDay_NasaPhoto/src/components/TodaysPicture/TodaysPicture.tsx
@@ -3,6 +3,13 @@ import { useEffect, useState } from "react";
 import { NasaPicture } from "../../routes/NasaPicture";
 
 import axios from "axios";
+import { Loader } from "../Loader/Loader";
+import { Box, Modal, Typography } from "@mui/material";
+
+import Button from '@mui/material/Button';
+import HdIcon from '@mui/icons-material/Hd';
+import InfoRoundedIcon from '@mui/icons-material/InfoRounded';
+
 
 const apiKey = "OmKd3TuQFb2uV48b870JL5Z3AmXY8GjKxnALBD0N";
 
@@ -11,7 +18,23 @@ export interface ITodaysPictureProps {
 }
 
 export function TodaysPicture (props: ITodaysPictureProps) {
+  const style = {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 400,
+    bgcolor: 'black',
+    border: '2px solid #000',
+    boxShadow: 24,
+    p: 4,
+  };
+
+
   const [url, setUrl] = useState("")
+  const [title, setTitle] = useState("")
+  const [info, setInfo] = useState("")
+  const [disabled, setDisabled] = useState("")
   const getTodayData = async () => {
   
     try {
@@ -20,6 +43,8 @@ export function TodaysPicture (props: ITodaysPictureProps) {
       
       const data = response.data;
       setUrl(data.hdurl)
+      setTitle(data.title)
+      setInfo(data.info)
       
     } catch (error) {
       console.log(error);
@@ -27,12 +52,30 @@ export function TodaysPicture (props: ITodaysPictureProps) {
   
   };
   useEffect(() => {
-    
-    getTodayData();
-  }, []);
+    const fetchData = async () => {
+      setDisabled("");
+      await getTodayData();
+      setTimeout(() => { setDisabled("disabled") }, 2500);
+    };
+    fetchData();
+  }, [url]);
   return (
     <>
-      <NasaPicture imageUrl={url}/>
+      <section className="my-today-section">
+        <h1>{title}</h1>
+        <div className="container-img">
+          
+          <NasaPicture imageUrl={url}/>
+        </div>
+            
+      </section>
+              
+              
+
+
+
+      <Loader disabled={disabled} />
+      
     </>
   );
 }
